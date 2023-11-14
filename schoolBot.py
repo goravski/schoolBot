e36f92c733e9ba7ff4b33a42f98e918f590aa259
Evgen Goravski <evgoravsky@gmail.com>
2023-11-14 14:02:21 +0300

    refactored: 1. change actions for command '/start' and added ReplyButton 2. added handler for text

--- schoolBot.py
+++ schoolBot.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 import constants
 import logging as log
 import schoolsAuth
@@ -17,9 +18,22 @@ user = schoolsAuth.UserAthenticationData()
 
 @bot.message_handler(commands=["start"])
 def start_message(message):
-    msg = bot.reply_to(message, "Введите логин:")
-    log.info(f"Request login of {message.from_user.username}")
-    bot.register_next_step_handler(msg, authantication_login)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button = types.KeyboardButton("Обновить")
+    markup.add(button)
+    text = (
+        "Добрый день!\n Для получения дневника текущей недели нажмите кнопку <Обновить>"
+        + "и введите по запросу логин и пароль для входа на сайт schools.by"
+    )
+    bot.send_message(message.chat.id, text=text, reply_markup=markup)
+
+
+@bot.message_handler(content_types="text")
+def message_teply(message):
+    if message.text == "Обновить":
+        msg = bot.reply_to(message, "Введите логин:")
+        log.info(f"Request login of {message.from_user.username}")
+        bot.register_next_step_handler(msg, authantication_login)
 
 
 def authantication_login(message):
@@ -39,9 +53,21 @@ def authantication_password(message):
         bot.send_message(
             message.chat.id, f"{user.data['username'] }, ждите ответа от сервера..."
         )
-        log.info(f"Login and password saved {message.from_user.username}")
-        hre = fp.get_dairy_page(user)
-        print(f"HREF :{hre}")
+        log.info(
+            f"authantication_password - Login and password saved {message.from_user.username}"
+        )
+        dairy = fp.get_dairy_page(user)
+        log.info(f"authantication_password - got dairy")
+        text = fp.transform_dict_to_text(dairy)
+        log.info(f"authantication_password - got text {text}")
+        bot.send_message(message.chat.id, text=text)
+        log.info(f"Send day list: {text}")
+        bot.send_message(
+            message.chat.id,
+            text="Для получения обновленной информации нажмите кнопку <Обновить>"
+            + "или введите текст 'Обновить' и повторно введите логин и пароль",
+        )
+
     except Exception as e:
         bot.reply_to(message, f"Exception in authantication_password {e}")
 
